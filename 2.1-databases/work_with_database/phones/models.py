e40f58eef82b64fe9d263e5509466bfe6674db3b
AlekseiZhmetko <a.zhmetko@gmail.com>
2022-12-04 20:44:03 +0300

    netology homework by Aleksei Zhmetko

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(default=None, primary_key=True)
+    name = models.CharField(default=None, max_length=100)
+    price = models.DecimalField(default=None, max_digits=18, decimal_places=1)
+    image = models.ImageField(default=None, upload_to='phones/phone_images')
+    release_date = models.DateField(default=None, auto_now=False, auto_now_add=False)
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField(default=None)
+
+
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render, redirect
-
+from .models import Phone
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sort = request.GET.get('sort', 'id')
+    phone_info = Phone.objects.all().order_by('id')
+    if sort == 'name':
+        phone_info = Phone.objects.all().order_by('name')
+    elif sort == 'min_price':
+        phone_info = Phone.objects.all().order_by('price')
+    elif sort == 'max_price':
+        phone_info = Phone.objects.all().order_by('-price')
     template = 'catalog.html'
-    context = {}
+    context = {'phones': phone_info}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {'phone': Phone.objects.get(slug=slug)}
     return render(request, template, context)
