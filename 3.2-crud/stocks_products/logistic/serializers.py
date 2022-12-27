d15a0a9571fa04bc26208788b923352841e4e967
Aleksei Zhmetko <a.zhmetko@gmail.com>
2022-12-27 22:46:35 +0300

    netology homework commit 1

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,34 @@
 from rest_framework import serializers
 
+from logistic.models import Product, Stock, StockProduct
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
+
+    def destroy(self, instance):
+        return instance.delete()
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -22,22 +36,24 @@ class StockSerializer(serializers.ModelSerializer):
 
         # создаем склад по его параметрам
         stock = super().create(validated_data)
-
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for position in positions:
+            position['stock'] = stock
+            StockProduct.objects.create(**position)
         return stock
 
-    def update(self, instance, validated_data):
+    def update_or_create(self, instance, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
 
         # обновляем склад по его параметрам
-        stock = super().update(instance, validated_data)
-
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        stock = super().update_or_create(instance, positions)
+        for position in positions:
+            position['stock'] = stock
+            StockProduct.objects.update_or_create(defaults=instance, positions)
         return stock
+
+
+# create_or_update() не работает - разобраться:
+# AssertionError: The `.create()` method does not support writable nested fields by default.
+# Write an explicit `.create()` method for serializer `logistic.serializers.StockSerializer`,
+# or set `read_only=True` on nested serializer fields.
